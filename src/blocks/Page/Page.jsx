5f6e6eaab0aa9afa3055619db2f8dcde4cb7bd6f
Sebastian Galustyan <sebastian@7digit.io>
2022-11-10 20:34:02 +0000

    Refactor: Align Global Admin UI to the new design system and initialise translation handling capabilities

---
+++ src/blocks/Page/Page.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Navbar, Icon } from "@USupport-components-library/src";
+import classNames from "classnames";
+import { useTranslation } from "react-i18next";
+
+import "./page.scss";
+
+/**
+ * Page
+ *
+ * Page wrapper
+ *
+ * @return {jsx}
+ */
+export const Page = ({
+  additionalPadding,
+  showNavbar,
+  showGoBackArrow,
+  heading,
+  headingButton,
+  classes,
+  children,
+}) => {
+  const { t, i18n } = useTranslation("page");
+  const pages = [
+    { name: t("page_1"), url: "/", exact: true },
+    { name: t("page_2"), url: "/providers" },
+    { name: t("page_3"), url: "/countries" },
+    { name: t("page_4"), url: "/content" },
+  ];
+
+  return (
+    <>
+      {showNavbar && (
+        <Navbar
+          pages={pages}
+          showProfile
+          yourProfileText={t("your_profile_text")}
+          i18n={i18n}
+        />
+      )}
+      <div
+        className={[
+          "page",
+          `${additionalPadding ? "" : "page--no-additional-top-padding"}`,
+          `${classNames(classes)}`,
+        ].join(" ")}
+      >
+        {(heading || showGoBackArrow || headingButton) && (
+          <div className="page__header">
+            {showGoBackArrow && (
+              <Icon
+                classes="page__header-icon"
+                name="arrow-chevron-back"
+                size="md"
+                color="#20809E"
+              />
+            )}
+            {heading && <h3 className="page__header-heading">{heading}</h3>}
+            {headingButton && headingButton}
+          </div>
+        )}
+        {children}
+      </div>
+    </>
+  );
+};
+
+Page.propTypes = {
+  /**
+   * Additional padding on top of the page
+   */
+  additionalPadding: PropTypes.bool,
+
+  /**
+   * Show the navbar
+   */
+  showNavbar: PropTypes.bool,
+
+  /**
+   * Show the go back arrow
+   */
+  showGoBackArrow: PropTypes.bool,
+
+  /**
+   * Heading text
+   */
+  heading: PropTypes.string,
+
+  /**
+   * Heading button
+   */
+  headingButton: PropTypes.node,
+
+  /**
+   * Additional classes
+   */
+  classes: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+  ]),
+};
+
+Page.defaultProps = {
+  additionalPadding: true,
+  showNavbar: true,
+  showGoBackArrow: true,
+};
