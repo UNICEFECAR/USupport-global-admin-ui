6b9d647d1bd83a7acb403998ea534e4bf75ddde1
Georgi 7DIGIT <georgi@7digit.io>
2022-12-01 23:26:27 +0200

    Create: hooks to get country and global admins

---
+++ src/hooks/useGetAllGlobalAdmins.js
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import { adminSvc } from "@USupport-components-library/services";
+
+export default function useGetAllGlobalAdmins() {
+  const fetchData = async () => {
+    const response = await adminSvc.getAllGlobalAdmins();
+
+    const formattedData = [];
+    for (const admin of response.data) {
+      formattedData.push({
+        adminId: admin.admin_id,
+        name: admin.name,
+        surname: admin.surname,
+        email: admin.email,
+        phone: admin.phone,
+        role: admin.role,
+        isActive: admin.is_active,
+      });
+    }
+
+    return formattedData;
+  };
+  const globalAdminsQuery = useQuery(["global-admins"], fetchData, {
+    onError: (error) => console.log(error, "error"),
+  });
+  return globalAdminsQuery;
+}
+export { useGetAllGlobalAdmins };
