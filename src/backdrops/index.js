098b655629f9287749c8738a51703cdc710dbe69
Georgi 7DIGIT <georgi@7digit.io>
2023-01-27 19:39:55 +0200

    Add: two factor authentication to the login

--- src/backdrops/index.js
+++ src/backdrops/index.js
@@ -1,3 +1,4 @@
 export * from "./CreateContentAdmin";
 export * from "./CreateLocalAdmin";
 export * from "./ChangePassword";
+export * from "./CodeVerification";
--- src/backdrops/locales.js
+++ src/backdrops/locales.js
@@ -1,3 +1,4 @@
 export * as CreateContentAdmin from "./CreateContentAdmin/locales.js";
 export * as CreateLocalAdmin from "./CreateLocalAdmin/locales.js";
 export * as ChangePassword from "./ChangePassword/locales.js";
+export * as CodeVerification from "./CodeVerification/locales.js";
