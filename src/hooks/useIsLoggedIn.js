e8076d57374cadad2c03651adcd4cffa982e71ad
Georgi 7DIGIT <georgi@7digit.io>
2023-02-22 03:25:09 +0200

    Fix: bug in useIsLoggedIn

--- src/hooks/useIsLoggedIn.js
+++ src/hooks/useIsLoggedIn.js
@@ -35,6 +35,8 @@ export const useIsLoggedIn = () => {
       }
     },
     onError: () => {
+      localStorage.removeItem("isRefreshingToken");
+
       setResult(false);
     },
     retry: false,
