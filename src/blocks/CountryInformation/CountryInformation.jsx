e929d3d05b600d5f91ddf5532d66b5ff120f7161
Georgi 7DIGIT <georgi@7digit.io>
2023-09-15 11:38:10 +0300

    Fix: styling

--- src/blocks/CountryInformation/CountryInformation.jsx
+++ src/blocks/CountryInformation/CountryInformation.jsx
@@ -82,12 +82,10 @@ export const CountryInformation = ({
     const status = admin.isActive ? "active" : "inactive";
     const statusLabel = admin.isActive ? "active" : "disabled";
     return [
-      <React.Fragment key={admin.email}>
-        <p className="text">{admin.name}</p>,
-        <StatusBadge label={t(statusLabel)} status={status} />,
-        <p className="text">{admin.email}</p>,
-        <p className="text">{admin.phone}</p>,
-      </React.Fragment>,
+      <p className="text">{admin.name}</p>,
+      <StatusBadge label={t(statusLabel)} status={status} />,
+      <p className="text">{admin.email}</p>,
+      <p className="text">{admin.phone}</p>,
     ];
   });
 
