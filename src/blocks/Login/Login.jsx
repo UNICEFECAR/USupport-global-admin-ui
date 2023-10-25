3fe79d920580fee06df3089531a26b30f1257dd9
Chavdar Shishkov <chavdar.shishkov123@gmail.com>
2023-10-25 15:05:04 +0300

    Refactor: Add logo to login

--- src/blocks/Login/Login.jsx
+++ src/blocks/Login/Login.jsx
@@ -10,6 +10,7 @@ import {
   InputPassword,
   Button,
 } from "@USupport-components-library/src";
+import { logoVerticalSvg } from "@USupport-components-library/assets";
 
 import "./login.scss";
 
@@ -40,6 +41,17 @@ export const Login = ({ data, setData, handleLogin, errors, isLoading }) => {
     <Block classes="login">
       <Grid md={8} lg={12} classes="login__grid">
         <GridItem md={8} lg={12} classes="login__grid__inputs-item">
+          <div className="login__grid__logo-item">
+            <h2 className="welcome__grid__logo-item__heading">
+              {t("heading")}
+            </h2>
+            <img
+              src={logoVerticalSvg}
+              alt="Logo"
+              className="welcome__grid__logo-item__logo"
+            />
+            <h2 className="welcome__grid__logo-item__heading">{t("admin")}</h2>
+          </div>
           <form onSubmit={handleLogin}>
             <Input
               label={t("email_label")}
